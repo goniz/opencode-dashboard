5874f54df6a671b57f9f327fc4b41fdc7fb0211a
Goni Zahavy <goni1993@gmail.com>
2025-07-23 21:39:42 +0300

    Task attempt eca7935a-155d-48ff-ae31-228b9cde4856 - Final changes

--- src/app/chat/page.tsx
+++
@@ -1,9 +0,0 @@
-import OpenCodeChatInterface from "@/components/opencode-chat-interface";
-
-export default function ChatPage() {
-  return (
-    <div className="h-screen">
-      <OpenCodeChatInterface />
-    </div>
-  );
-}
